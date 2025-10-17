32fe7565ca3a31619d25e72b5554aa9be15903e6
ZAIN ALI <zaincui5542@gmail.com>
2025-10-17 12:28:41 +0500

    commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,10 @@
 import { Background } from "@/components/background";
-import { FAQ } from "@/components/blocks/faq";
-import { Features } from "@/components/blocks/features";
 import { Hero } from "@/components/blocks/hero";
-import { Logos } from "@/components/blocks/logos";
-import { Pricing } from "@/components/blocks/pricing";
-import { ResourceAllocation } from "@/components/blocks/resource-allocation";
-import { Testimonials } from "@/components/blocks/testimonials";
 
 export default function Home() {
   return (
-    <>
-      <Background className="via-muted to-muted/80">
-        <Hero />
-        <Logos />
-        <Features />
-        <ResourceAllocation />
-      </Background>
-      <Testimonials />
-      <Background variant="bottom">
-        <Pricing />
-        <FAQ />
-      </Background>
-    </>
+    <Background className="via-muted to-muted/80">
+      <Hero />
+    </Background>
   );
 }
